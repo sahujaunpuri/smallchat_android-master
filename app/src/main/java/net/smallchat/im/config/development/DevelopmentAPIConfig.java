44f46f7f3c5ed57c6487b3c8b56fa6a511612be4
sean <present810209@gmail.com>
2018-09-13 17:58:43 +0800

    aaa

--- app/src/main/java/net/smallchat/im/config/development/DevelopmentAPIConfig.java
+++ app/src/main/java/net/smallchat/im/config/development/DevelopmentAPIConfig.java
@@ -4,7 +4,7 @@ import net.smallchat.im.config.AppConfig;
 
 public class DevelopmentAPIConfig {
 
-    //APP接口配置信息    
+    //APP接口配置信息
     //预留APPKEY 接口安全验证.目前已经加入到每一个接口请求中  这个目前后台没有验证
     public static final String APPKEY ="0e93f53b5b02e29ca3eb6f37da3b05b9";
     //存储服务器
